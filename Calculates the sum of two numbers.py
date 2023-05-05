06417d88dbf188e185e74ca75ffd96d59140ba4c
hsnur <116380846+hsnur@users.noreply.github.com>
2023-05-06 01:32:57 +0300

    Add files via upload

---
+++ Calculates the sum of two numbers.py
@@ -0,0 +1,9 @@
+# prompt user for two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# calculate the sum of the two numbers
+sum = num1 + num2
+
+# print the result
+print("The sum of", num1, "and", num2, "is", sum)
